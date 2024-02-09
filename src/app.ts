c106874e99ee64cde4d2d746355922d0ea086a2c
Breno Salomão Silva <brenosalomaosilva@gmail.com>
2024-02-09 11:23:07 -0300

    adicionando classes e comentários

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,44 @@ import cors from 'cors';
 import { Ave } from './model/Ave';
 import { Reptil } from './model/Reptil';
 import { Mamifero } from './model/Mamifero';
-import { Animal } from './model/Animal';
+import { Atracao } from './model/Atracao';
+import { Zoologico } from './model/Zoologico';
+import { Habitat } from './model/Habitat';
 
 const server = express();
-const port = 3000
+const port: number = 3000;
 
 server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-    let ave: Ave = new Ave(90, 'Pinto', 4, 'Feminino'); 
-    let mamifero: Mamifero = new Mamifero('Border Collie', 'Burder', 2, 'Masculino');
-    let reptil: Reptil = new Reptil('Ganoide', 'Romeu', 17, 'Masculino');
-    res.json([ave, mamifero, reptil]);
-})
+   let ave: Ave= new Ave(10, 'Calopsita', 11, 'Masculino');
+   let reptil: Reptil= new Reptil ('placóides', 'Cobra', 15, 'Masculino');
+   let mamifero: Mamifero= new Mamifero ('canguru', 'Julia', 12, 'Feminino');
+
+    res.json([ave,reptil, mamifero]);
+});
+
+server.post('/habitat', (req, res) => {
+    const {nome, animais}= req.body;
+    const habitat = new Habitat(nome, animais);
+    console.log(habitat)
+    res.status(200).json('Habitat criado');
+});
+
+server.post('/atracao', (req, res) => {
+    const {nome, habitat}= req.body;
+    const atracao = new Atracao(nome, habitat);
+    console.log(atracao)
+    res.status(200).json('Atrção criada');
+});
+
+server.post('/zoologico', (req, res) => {
+    const {nome, atracao}= req.body;
+    const zoo = new Zoologico (nome, atracao);
+    console.log(zoo);
+    res.status(200).json('Zoologico criado');
+});
 
 server.post('/cadastrarAve', (req, res) => {
     const { envergadura, nome, idade, genero } = req.body;
