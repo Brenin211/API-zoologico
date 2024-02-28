82ab8e00c6e0f94f10cc9cbaad00f990d4b4a018
Breno Salomão Silva <brenosalomaosilva@gmail.com>
2024-02-28 14:55:38 -0300

    atuaçizando código ;)

--- src/model/Animal.ts
+++ src/model/Animal.ts
@@ -1,72 +1,135 @@
+import { DatabaseModel } from "./DatabaseModel";
+
 /**
- * Classe que representa um Animal.
+ * Pool de conexão do banco de dados
+ */
+const database = new DatabaseModel().pool;
+
+/**
+ * Representa um animal no zoológico.
  */
 export class Animal {
-    // Propriedades privadas que armazenam informações sobre o animal.
-    private nome: string;
-    private idade: number;
-    private genero: string;
 
     /**
-     * Construtor da classe Animal.
+     * O nome do animal.
+     */
+    private nomeAnimal: string;
+
+    /**
+     * A idade do animal.
+     */
+    private idadeAnimal: number;
+
+    /**
+     * O gênero do animal (ex: "macho", "fêmea", "desconhecido").
+     */
+    private generoAnimal: string;
+
+    /**
+     * Cria uma nova instância de Animal.
+     * 
      * @param _nome O nome do animal.
      * @param _idade A idade do animal.
      * @param _genero O gênero do animal.
      */
-    constructor(_nome: string, _idade: number, _genero: string) {
-        // Inicializa as propriedades do animal com os valores fornecidos.
-        this.nome = _nome;
-        this.idade = _idade;
-        this.genero = _genero;
+    constructor(_nome: string,
+        _idade: number,
+        _genero: string) {
+        this.nomeAnimal = _nome;
+        this.idadeAnimal = _idade;
+        this.generoAnimal = _genero;
     }
 
     /**
      * Obtém o nome do animal.
+     * 
      * @returns O nome do animal.
      */
-    public getNome(): string {
-        return this.nome;
+    public getNomeAnimal(): string {
+        return this.nomeAnimal;
     }
 
     /**
      * Define o nome do animal.
+     * 
      * @param nome O nome a ser atribuído ao animal.
      */
-    public setNome(nome: string): void {
-        this.nome = nome;
+    public setNomeAnimal(nome: string): void {
+        this.nomeAnimal = nome;
     }
 
     /**
      * Obtém a idade do animal.
+     * 
      * @returns A idade do animal.
      */
-    public getIdade(): number {
-        return this.idade;
+    public getIdadeAnimal(): number {
+        return this.idadeAnimal;
     }
 
     /**
      * Define a idade do animal.
+     * 
      * @param idade A idade a ser atribuída ao animal.
      */
-    public setIdade(idade: number): void {
-        this.idade = idade;
+    public setIdadeAnimal(idade: number): void {
+        this.idadeAnimal = idade;
     }
 
     /**
      * Obtém o gênero do animal.
+     * 
      * @returns O gênero do animal.
      */
-    public getGenero(): string {
-        return this.genero;
+    public getGeneroAnimal(): string {
+        return this.generoAnimal;
     }
 
     /**
      * Define o gênero do animal.
+     * 
      * @param genero O gênero a ser atribuído ao animal.
      */
-    public setGenero(genero: string): void {
-        this.genero = genero;
+    public setGeneroAnimal(genero: string): void {
+        this.generoAnimal = genero;
+    }
+
+    /**
+     * Retorna uma lista com todos os animais cadastrados no sistema.
+     * @returns lista com os animais cadastrados no sistema
+     */
+    static async listarTodosAnimais(): Promise<any> {
+        try {
+            // Query para a consulta no banco de dados
+            const selectAllQuery = `SELECT
+                                        a.idAnimal,
+                                        a.nomeAnimal,
+                                        a.idadeAnimal,
+                                        a.generoAnimal,
+                                        CASE
+                                            WHEN av.idAve IS NOT NULL THEN 'Ave'
+                                            WHEN m.idMamifero IS NOT NULL THEN 'Mamifero'
+                                            WHEN r.idReptil IS NOT NULL THEN 'Reptil'
+                                        END AS tipoAnimal,
+                                        av.envergadura,
+                                        m.especie,
+                                        r.tipoDeEscamas
+                                    FROM
+                                        Animal a
+                                    LEFT JOIN
+                                        Ave av ON a.idAnimal = av.idAve
+                                    LEFT JOIN
+                                        Mamifero m ON a.idAnimal = m.idMamifero
+                                    LEFT JOIN
+                                        Reptil r ON a.idAnimal = r.idReptil;
+            `
+            // Executa a query e retorna o resultado para quem chamou a função
+            return await database.query(selectAllQuery);
+        } catch (error) {
+            // Caso dê algum erro na query do banco, é lançado o erro para quem chamou a função
+            console.log('Erro no modelo');
+            console.log(error);
+            return "error, verifique os logs do servidor";
+        }
     }
 }
-
-
--- src/model/Ave.ts
+++ src/model/Ave.ts
@@ -1,16 +1,21 @@
 import { Animal } from "./Animal";
 import { DatabaseModel } from "./DatabaseModel";
 
+/**
+ * Pool de conexão do banco de dados
+ */
 const database = new DatabaseModel().pool;
 
 /**
- * Representa uma ave no zoológico, que é uma subclasse de Animal.
+ * Representa uma ave no zoológico.
+ * Estende a classe Animal.
  */
 export class Ave extends Animal {
+
     /**
-     * A envergadura da ave.
+     * A envergadura da ave (em centímetros).
      */
-    private envergadura: string;
+    private envergadura: number;
 
     /**
      * Cria uma nova instância de Ave.
@@ -20,11 +25,13 @@ export class Ave extends Animal {
      * @param _genero O gênero da ave.
      * @param _envergadura A envergadura da ave.
      */
-    constructor(_nome: string, 
-                _idade: number, 
-                _genero: string, 
-                _envergadura: string) {
+    constructor(_nome: string,
+        _idade: number,
+        _genero: string,
+        _envergadura: number) {
+        // Chama o construtor da classe pai (Animal)
         super(_nome, _idade, _genero);
+        // Atribui a envergadura fornecida ao atributo envergadura da ave
         this.envergadura = _envergadura;
     }
 
@@ -33,7 +40,7 @@ export class Ave extends Animal {
      * 
      * @returns A envergadura da ave.
      */
-    public getEnvergadura(): string {
+    public getEnvergadura(): number {
         return this.envergadura;
     }
 
@@ -42,43 +49,90 @@ export class Ave extends Animal {
      * 
      * @param _envergadura A envergadura a ser atribuída à ave.
      */
-    public setEnvergadura(_envergadura: string): void {
+    public setEnvergadura(_envergadura: number): void {
         this.envergadura = _envergadura;
     }
 
+    /**
+     * Retorna uma lista com todos as aves cadastradas no banco de dados
+     * 
+     * @returns Lista com todos as aves cadastradas no banco de dados
+     */
     static async listarAves() {
+        // Cria uma lista (array) vazia do tipo Ave
         const listaDeAves: Array<Ave> = [];
+
+        // Construção da query para selecionar as informações de um Ave
+        const querySelectAve = `SELECT Animal.idAnimal, Animal.nomeAnimal, Animal.idadeAnimal, Animal.generoAnimal, Ave.envergadura 
+                                    FROM Animal 
+                                    JOIN Ave ON Animal.idAnimal = Ave.idAve;`;
+
+
         try {
-            const queryReturn = await database.query(`SELECT * FROM ave`);
+            // Faz a consulta no banco de dados e retorna o resultado para a variável queryReturn
+            const queryReturn = await database.query(querySelectAve);
+            // Percorre todas as linhas da queryReturn e acessa cada objeto individualmente
             queryReturn.rows.forEach(ave => {
+                // Coloca o objeto dentro da lista de Aves
                 listaDeAves.push(ave);
             });
 
-            // só para testar se a lista veio certa do banco
-            console.log(listaDeAves);
-
+            // retorna a lista de Aves para quem chamou a função
             return listaDeAves;
         } catch (error) {
+            // Caso dê algum erro na query do banco, é lançado o erro para quem chamou a função
             console.log('Erro no modelo');
             console.log(error);
-            return "error";
+            return "error, verifique os logs do servidor";
         }
     }
 
+    /**
+     * Cadastra um objeto do tipo Ave no banco de dados
+     * 
+     * @param ave Objeto do tipo Ave
+     * @returns **true** caso sucesso, **false** caso erro
+     */
     static async cadastrarAve(ave: Ave): Promise<any> {
+        // Cria uma variável do tipo booleano para guardar o status do resultado da query
+        let insertResult = false;
+
         try {
-            let insertResult = false;
-            await database.query(`INSERT INTO ave (nome, idade, genero, envergadura)
-                VALUES
-                ('${ave.getNome().toUpperCase()}', ${ave.getIdade()}, '${ave.getGenero().toUpperCase()}', '${ave.getEnvergadura().toUpperCase()}');
-            `).then((result) => {
-                if(result.rowCount != 0) {
-                    insertResult = true;
-                }
-            });
+            // Construção da query para inserir as informações de um Ave. A query irá retornar o ID gerado para o animal pelo banco de dados
+            const queryInsertAnimal = `INSERT INTO animal (nomeAnimal, idadeAnimal, generoAnimal) 
+                                        VALUES 
+                                        ('${ave.getNomeAnimal().toUpperCase()}', ${ave.getIdadeAnimal()}, '${ave.getGeneroAnimal().toUpperCase()}')
+                                        RETURNING idAnimal;`;
+
+            // Faz a query de insert no banco de dados, passando para o banco as informações do objeto recebibo como parâmetro pela função
+            await database.query(queryInsertAnimal)
+                // Testa para ter certeza que foi possível inserir os dados no banco
+                .then(async (result) => {
+                    const idAnimal = result.rows[0].idanimal;
+                    // Preparando a query para inserir a raça do mamífero no banco de dados
+                    const queryInsertAve = `INSERT INTO ave (idAve, envergadura)
+                                                VALUES
+                                                (${idAnimal}, ${ave.getEnvergadura()})`;
+
+                    // Faz a query de insert da raça do mamífero no banco de dados
+                    await database.query(queryInsertAve)
+
+                        // Testa para ter certeza que foi possível inserir os dados no banco
+                        .then((resultAve) => {
+                            if (resultAve.rowCount != 0) {
+                                // Se o número de linhas for diferente de zero, a operação deu certo e o valor VERDADEIRO é atribuido na variável
+                                insertResult = true;
+                            }
+                        });
+                });
+            // Retorna VERDADEIRO para quem chamou a função, indicando que a operação foi realizada com sucesso
             return insertResult;
-        } catch(error) {
-            return error;
+        } catch (error) {
+            // Imprime o erro no console
+            console.log(error);
+
+            // Caso a inserção no banco der algum erro, é restorno o valor FALSO para quem chamou a função
+            return insertResult;
         }
     }
 }
